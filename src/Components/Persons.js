7ffe7aa07215dea0b6faf1d9e872ce232ff4f355
DINESH S <dinisellappan@gmail.com>
2021-02-12 13:08:11 +0530

    life cycle update methodr

--- src/Components/Persons.js
+++ src/Components/Persons.js
@@ -3,6 +3,24 @@ import React from "react";
 import Person from "./Person";
 
 class Persons extends React.Component {
+  shouldComponentUpdate(nextProps, nextState) {
+    console.log("{Persons.js] shouldcomponentupdate", nextProps);
+    if (nextProps !== this.props) return true;
+    else return false;
+  }
+  getSnapshotBeforeUpdate(prevProps, prevState) {
+    console.log("[Persons.js] getsnapshotbeforeupdate", prevProps);
+    return {
+      pervious: {
+        Prevprops: prevProps,
+        Prevstate: prevState,
+      },
+    };
+  }
+  componentDidUpdate(prevProps, prevState, snapshot) {
+    console.log("[Persons.js} componentwillUpdate");
+    console.log(JSON.stringify(snapshot));
+  }
   render() {
     console.log("[persons.js] render ");
     return this.props.person.map((e, indx) => {
